9a9117b12e9ec3e4901bf07839eeb720f7013247
JaasielSG <jaasiel.sotoguerrero08@gmail.com>
2024-10-21 22:40:06 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,71 +1,16 @@
-// Arreglo para almacenar tareas
-let tareas = [];
-
-// Funcion para mostrar opciones
-function mostrarMenu(){
-    return parseInt(prompt(`
-        "Opciones disponibles"
-        1. Agregar tarea.
-        2. Ver todas las tareas.
-        3. Marcar tareas completadas.
-        4. Salir
-        `));
+function agregarPantalla(value){
+    document.getElementById("Pantalla").value+=value;
 }
+function limpiarPantalla(){
+    document.getElementById("Pantalla").value="";
+}
+function calcular(){
+    try{
+        let result =eval( document.getElementById("Pantalla").value);
+        document.getElementById("Pantalla").value = result;
 
-// Agregar tarea
-function agregarTarea(){
-    let nombre = prompt("Ingresa el nombre de la tarea");
-    if(nombre){
-        let tarea = {
-            nombre: nombre,
-            completada: false
-        };
-        tareas.push(tarea);
-        alert("Tarea agregada con éxito");
-    }else{
-        alert("El nombre de la tarea no puede estar vacío");
+    }catch (error){
+        document.getElementById("Pantalla").value = "Error";
+
     }
 }
-
-// Ver tareas
-function verTarea(){
-    if(tareas.length == 0){
-        alert("No hay tareas en la lista");
-    }else{
-        let mensaje = "Lista de tareas: \n";
-        tareas.forEach((tarea, index)=>{
-            
-        });
-    }
-}
-
-// Funcion principal
-function iniciarPrograma(){
-    let continuar = true;
-    while(continuar){
-        let opcion = mostrarMenu();
-        switch(opcion){
-            case 1:
-                agregarTarea();
-                break;
-            case 2:
-                verTarea();
-                break;
-            case 3:
-                marcarTarea();
-                break;
-            case 4:
-                alert("Saliendo del programa...");
-                continuar = false;
-                break;
-            default:
-                alert("Opción no valida");
-                break;
-
-        }
-    }
-    alert("Programa finalizado");
-
-}
-
-iniciarPrograma();
